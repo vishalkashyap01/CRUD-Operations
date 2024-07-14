bc59482a8b97e0a3fb6b174b9af542fac4493c11
Vishal Kashyap <147410216+vishalkashyap01@users.noreply.github.com>
2024-07-15 03:28:47 +0530

    updated Version 1.0.5
    Add 'update' function
    Add error handling
    add exit method to terminate from the console.

--- todo.py
+++ todo.py
@@ -1,49 +1,76 @@
-# version - 1.0.4
+# Version 1.0.5 - Todo List Manager
 
-def add(li):
+# Function to add a task to the todo list
+def add_task(todo_list):
     task = input("Write task to add: ")
-    li.append(task)
-    print("<< ", li, " >>")
-    make_list()
+    todo_list.append(task)
+    print("<< Updated list: ", todo_list, " >>")
+    make_list(todo_list)
 
-def remove(li):
+# Function to remove a task from the todo list
+def remove_task(todo_list):
     print('\n')
     erase = int(input("\nEnter task number to remove from the list: "))
-    if erase < len(li):
-        li.pop(erase)
-        print("<< ", li, " >>")
+    if erase < len(todo_list):
+        todo_list.pop(erase)
+        print("<< Updated list: ", todo_list, " >>")
     else:
         print("Invalid task number.")
-    make_list()
+    make_list(todo_list)
 
-def update(li):
+# Function to update a task in the todo list
+def update_task(todo_list):
     print('\n')
     task_no = int(input("\nEnter task number to update: "))
-    if task_no < len(li):
+    if task_no < len(todo_list):
         new_task = input("Enter new task: ")
-        li[task_no] = new_task
-        print("<< Updated list: ", li, " >>")
+        todo_list[task_no] = new_task
+        print("<< Updated list: ", todo_list, " >>")
     else:
         print("Invalid task number.")
-    make_list()
+    make_list(todo_list)
 
-def make_list():
-    choice = int(input("\n1 to add, 2 to remove, 3 to update, 0 to exit: "))
+# Function to view tasks in the todo list
+def view_tasks(todo_list):
+    print('\n')
+    if not todo_list:
+        print("Todo list is empty.")
+    else:
+        print("<< Current todo list: ", todo_list, " >>")
+    make_list(todo_list)
+
+# Function to display the menu and handle user input
+def make_list(todo_list):
+    print('\n')
+    print("Menu:")
+    print("1. Add a task")
+    print("2. Remove a task")
+    print("3. Update a task")
+    print("4. View tasks")
+    print("0. Exit")
+
+    choice = int(input("Enter your choice: "))
     if choice == 1:
-        add(todo)
+        add_task(todo_list)
     elif choice == 2:
-        remove(todo)
+        remove_task(todo_list)
     elif choice == 3:
-        update(todo)
+        update_task(todo_list)
+    elif choice == 4:
+        view_tasks(todo_list)
     elif choice == 0:
         print("\nExiting...")
         return
     else:
         print("\nInvalid choice.")
-        make_list()
+        make_list(todo_list)
 
-# main method
-todo = []
 
-print("Do you want to create a todo list?")
-make_list()
+# Main method to initialize todo list and start the application
+if __name__ == "__main__":
+    todo_list = []
+
+    print("Do you want to create a todo list?")
+    make_list(todo_list)
+
+# Author - vishalkashyap01
