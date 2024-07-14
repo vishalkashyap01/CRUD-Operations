4cceada62e9be44a9c472067ff15a2c976832342
Vishal Kashyap <147410216+vishalkashyap01@users.noreply.github.com>
2024-07-15 03:20:12 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,43 +1,49 @@
-# add task to list - function
-# version - 1.0.3
+# version - 1.0.4
 
 def add(li):
-    task = input("write task to add : ")
+    task = input("Write task to add: ")
     li.append(task)
     print("<< ", li, " >>")
     make_list()
 
-
-# tasks to delete from the list
 def remove(li):
     print('\n')
-    erase = int(input("\n Enter task no to remove from the list : "))
-    li.pop(erase)
-    print("<< ", li, " >>")
+    erase = int(input("\nEnter task number to remove from the list: "))
+    if erase < len(li):
+        li.pop(erase)
+        print("<< ", li, " >>")
+    else:
+        print("Invalid task number.")
     make_list()
 
+def update(li):
+    print('\n')
+    task_no = int(input("\nEnter task number to update: "))
+    if task_no < len(li):
+        new_task = input("Enter new task: ")
+        li[task_no] = new_task
+        print("<< Updated list: ", li, " >>")
+    else:
+        print("Invalid task number.")
+    make_list()
 
 def make_list():
-    choice = int(input("1 for add & 0 for not : "))
+    choice = int(input("\n1 to add, 2 to remove, 3 to update, 0 to exit: "))
     if choice == 1:
         add(todo)
-    elif choice == 0:
-        for a in todo:
-            print(a)
+    elif choice == 2:
         remove(todo)
-    else:
+    elif choice == 3:
+        update(todo)
+    elif choice == 0:
+        print("\nExiting...")
         return
-
+    else:
+        print("\nInvalid choice.")
+        make_list()
 
 # main method
 todo = []
 
-print("Are you want to make a todo list -->")
+print("Do you want to create a todo list?")
 make_list()
-
-for i in range(0, 4):
-    print(i+1, '. ', todo[i])
-make_list()
-
-for i in range(0, 4):
-    print(i, '. ', todo[i])
